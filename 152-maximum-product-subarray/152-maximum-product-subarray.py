e0d09eff7abef071a7ecd6c4af27df9a75473fcb
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2022-02-11 13:57:01 -0500

    Time: 161 ms (12.12%), Space: 14.4 MB (76.45%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -2,6 +2,8 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
             currMin = 1
             currMax = 1
+            
+            # We keep nums[0] here for the singular element case.
             answer = nums[0]
             for i in nums:
                 tempArray = [i, i * currMin, i * currMax]
