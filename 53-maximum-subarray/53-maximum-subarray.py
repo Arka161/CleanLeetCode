7de7641c2af5d2f619a89ec37ae45297d7aae4e8
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2022-03-11 17:17:01 -0500

    Time: 954 ms (55.49%), Space: 27.9 MB (86.38%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,14 +1,19 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        currentSum = nums[0]
-        bestSum = nums[0]
-        
+        basicSum = nums[0]
+        maxVal = nums[0]
+        # [-2,1,-3,4,-1,2,1,-5,4]
+        # Output: 6
+        #[1, 2, 3, 4]
         for i in range(1, len(nums)):
-            if currentSum >= 0:
-                currentSum += nums[i]
-            else:
-                currentSum = nums[i]
+            basicSum = nums[i] + basicSum
             
-            if currentSum > bestSum:
-                bestSum = currentSum
-        return bestSum
+            if basicSum < nums[i]:
+                basicSum = nums[i]
+            
+            #print("BASIC SUM HERE IS", basicSum)
+            maxVal = max(maxVal, basicSum)
+            #print("MAX VAL HERE IS", )
+            if basicSum < 0:
+                basicSum = 0
+        return maxVal
