bb2e38d0d3f6482e929443aae68988a20ef68246
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2022-04-04 01:42:38 -0400

    Time: 300 ms (94.68%), Space: 16.6 MB (43.47%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        rows = len(grid)
+        cols = len(grid[0])
         
-        count = 0
+        ans = 0
+        def dfs(grid, i, j):
+            if i >= rows or j >= cols or i < 0 or j < 0 or grid[i][j] != "1":
+                return
+            grid[i][j] = "#"
+            dfs(grid, i+1, j)
+            dfs(grid, i-1, j)
+            dfs(grid, i, j+1)
+            dfs(grid, i, j-1)
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
-                    count += 1
-        return count
-    
-    def dfs(self, grid, i, j): 
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '#'
-        self.dfs(grid, i + 1, j)
-        self.dfs(grid, i - 1, j)
-        self.dfs(grid, i, j + 1)
-        self.dfs(grid, i, j - 1)
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    dfs(grid, i, j)
+                    ans += 1
+            
+            
+        return ans
