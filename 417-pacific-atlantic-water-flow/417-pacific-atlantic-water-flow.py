964b02430e71ec6f554adbe123e46d9fb76fe72a
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2022-04-04 03:18:47 -0400

    Time: 584 ms (17.73%), Space: 15.8 MB (36.72%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,52 +1,43 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if len(heights) == 0:
-            return 
+        rows = len(heights)
+        cols = len(heights[0])
         
-        # Row
-        m = len(heights)
-        
-        # Column
-        n = len(heights[0])
-        
-        atlantic = [[False for x in range(n)] for y in range(m)] 
-        pacific = [[False for x in range(n)] for y in range(m)]
+        atlantic = [[False for x in range(cols)] for y in range(rows)]
+        pacific = [[False for x in range(cols)] for y in range(rows)]
         
         ans = []
-        visited = [[False for x in range(n)] for y in range(m)] 
+        
+        visited = [[False for x in range(cols)] for y in range(rows)]
         
         def dfs(mat, isPacific, i, j):
-            
             if isPacific:
                 visited = atlantic 
-            else: 
+            else:
                 visited = pacific
-
+                
             if visited[i][j]:
                 return
             visited[i][j] = True
-            
             if atlantic[i][j] and pacific[i][j]:
                 ans.append([i, j])
-            if i + 1 < m and mat[i + 1][j] >= mat[i][j]:
-                dfs(mat, isPacific, i+1, j)
-            
+                
+            if i + 1 < rows and mat[i + 1][j] >= mat[i][j]:
+                dfs(mat, isPacific, i + 1, j)
+
             if i - 1 >= 0 and mat[i - 1][j] >= mat[i][j]:
                 dfs(mat, isPacific, i - 1, j)
             
-            if j + 1 < n and mat[i][j + 1] >= mat[i][j]:
+            if j + 1 < cols and mat[i][j + 1] >= mat[i][j]:
                 dfs(mat, isPacific, i, j + 1)
                 
             if j - 1 >= 0 and mat[i][j - 1] >= mat[i][j]:
                 dfs(mat, isPacific, i, j - 1)
-        
-        for i in range(0, m):
+        for i in range(0, rows):
             dfs(heights, True, i, 0)
-            dfs(heights, False, i, n-1)
-        for i in range(0, n):
+            dfs(heights, False, i, cols-1)
+        for i in range(0, cols):
             dfs(heights, True, 0, i)
-            dfs(heights, False, m - 1, i)
+            dfs(heights, False, rows - 1, i)
             
         return ans
-            
-        
