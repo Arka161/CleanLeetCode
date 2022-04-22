b6103c494028d5a583a5f20e6dcabb35dd102b11
Arka Mukherjee <arka_mukherjee@srmuniv.edu.in>
2022-04-22 04:51:26 -0400

    Time: 254 ms (74.92%), Space: 22.3 MB (25.06%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,21 +5,26 @@ class Solution:
         # Form Right Array - [24,12, 4, 1]
         
         left_arr = [0] * len(nums)
-        
-        left_arr[0] = 1
-        
-        for index in range(1, len(nums)):
-            left_arr[index] = left_arr[index - 1] * nums[index - 1]
-            
         right_arr = [0] * len(nums)
         
-        right_arr[len(nums) - 1] = 1
+        left_val = 1
+        for index, val in enumerate(nums):
+            left_arr[index] = left_val
+            left_val *= nums[index]
         
-        for index in range(len(nums) - 1, 0, -1):
-            right_arr[index - 1] = right_arr[index] * nums[index]
+        right_val = 1
+        index_b = len(nums) - 1
+        for index in range(len(nums) - 1, -1, -1):
+            right_arr[index_b] = right_val
+            index_b -= 1
+            right_val *= nums[index]
         
-        return [num1 * num2 for num1, num2 in zip(left_arr, right_arr)]
+        final_arr = [0] * len(nums)
         
         
+        for i in range(len(nums)):
+            final_arr[i] = right_arr[i] * left_arr[i]
+            
+        return final_arr
         
         
